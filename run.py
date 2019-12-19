af190ccc549b5fdef0c591c3fa05c8fee8a0599d
Seth <seth.c.hannah@gmail.com>
2019-12-19 13:15:28 -0500

    add attack (get weap info and calc chance of hitting) add hit (get weap info and calc damage) add skillcheck (get skill info and calc skill check roll) add cli menu in run() add eweapon default val of "Ranseur"

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 
 
-import json, os
+import json, os, re
 from rofi import Rofi
+import pyperclip
 
 def getBasicInfo(data):
     ret = {}
@@ -36,12 +37,13 @@ def getWeaponInfo(data):
                 try:
                     value[suffix] = data[weaponstr + suffix]
                 except:
-                    print (key + " modifier " + suffix + " not found.")
-
+                    # print (key + " modifier " + suffix + " not found.")
+                    pass
             #set value to dict containing the info
             ret[key] = value
         except:
-            print(weaponstr + " not found.")
+            # print(weaponstr + " not found.")
+            pass
     return ret
 
 def getSkillInfo(data):
@@ -62,26 +64,160 @@ def getSkillInfo(data):
                 try:
                     value[suffix] = data[skillstr + suffix]
                 except:
-                    print (key + " modifier " + suffix + " not found.")
+                    # print (key + " modifier " + suffix + " not found.")
+                    pass
 
 
             #set value to dict containing the info
             ret[key] = value
         except:
-            print(skillstr + " not found.")
+            # print(skillstr + " not found.")
+            pass
     return ret
 
-def attack(weapon, stats):
+def getAbilityInfo(data):
+    ret = {}
+    #possible keys (abilities)
+    keys = ["Str", "Dex", "Con", "Int", "Wis", "Cha"]
+    values = ["Score", "Mod"]
+    for key in keys:
+        #use try catch incase ability not available
+        try:
+            #set key to be name of the skill
+            value = {}
+            value[values[0]] = data[key]
+            value[values[1]] = data[key + "Mod"]
+
+            #set value to dict containing the info
+            ret[key] = value
+        except:
+            print(key + " not found.")
+    return ret
+
+def attack(data, weapon):
+    #get weapon Type
+    wtype, wab = getWeaponInfo(data)[weapon]["Type"], getWeaponInfo(data)[weapon]["AB"]
+    #get strength and dexterity modifiers
+    str, dex = getAbilityInfo(data)["Str"]["Mod"], getAbilityInfo(data)["Dex"]["Mod"]
+    # if weapon type is melee set mod to str mod
+    if wtype.strip().lower() == "melee":
+        mod = str
+    # if weapon type is ranged set mod to dex mod
+    elif wtype.strip().lower() == "ranged" or wtype.strip().lower() == "range":
+        mod = dex
+    # if weapon type is unknown, ask user for entry
+    else:
+        print("unknown weapon type")
+        uc = input ("melee or ranged (m/r) -> ")
+        if uc == "m":
+            mod = str
+        elif uc == "r":
+            mod = dex
+
+    string = "/r 1d20"
+    regex = re.match(r"\+\s*(\d*)", wab.strip())
+    if (regex != None):
+        totmod = int(regex.group(0)) + int(mod)
+        string += " + {}".format(totmod)
+    return string
+
+def hit(data, weapon):
+    wi = getWeaponInfo(data)[weapon]
+    wtype, wab = wi["Type"], wi["AB"]
+    #get strength modifier
+    str = getAbilityInfo(data)["Str"]["Mod"]
+    # if weapon type is melee set mod to str mod
+    if wtype.strip().lower() == "melee":
+        mod = str
+    # if weapon type is ranged set mod to 0
+    elif wtype.strip().lower() == "ranged" or wtype.strip().lower() == "range":
+        mod = 0
+    # [num]d[num]+[num]
+    # ex. 1d10+3
+    regex = re.search(r"(\d*)\s*[d,D]\s*(\d*)\s*\+*\s*(\d*)", wi["Damage"])
+    string = "/r "
+    #print (wi["Damage"])
+    if regex.group(1) != None:
+        #print (regex.group(1))
+        string += regex.group(1)
+    if regex.group(2) != None:
+        #print (regex.group(2))
+        string += "d" + regex.group(2)
+    if regex.group(3) != "":
+        #print (regex.group(3))
+        string += " + {}".format(int(mod) + int(regex.group(3)))
+    return string
+
+def skillcheck(data):
+    for x in range (2):
+        uc = input ("skill -> ")
+        str = "/r 1d20 "
+        if uc == "":
+            for skill in getSkillInfo(data):
+                print (skill)
+        else:
+            for skill in getSkillInfo(data):
+                if uc.lower() in skill.lower():
+                    str += "+ {}".format(getSkillInfo(data)[skill]["Mod"])
+                    break
+            return str
+
+def save(data):
     pass
-    
+
+
 def run():
     with open("Seth's Marshal-2019-12-19.json", "r") as readfile:
         data = json.load(readfile)
 
     # grab only the important data for this program
     data = data["sheet_data"]["jsondata"]
-    print (getWeaponInfo(data))
-    rofi(data)
+
+    # initialize equipped weapon
+    eweapon = ""
+    eweapon = "Ranseur"
+
+    while (True):
+
+        command = input("-> ")
+        #check to make sure weapon is equipped
+        if eweapon != "":
+            # attack roll, ask for hit after
+            if command in "attack" or command == "":
+                pyperclip.copy(attack (data, eweapon))
+                uc = input ("hit?(y/N) -> ")
+                if uc.lower() == 'y':
+                    pyperclip.copy(hit(data, eweapon))
+                continue
+
+            # hit roll
+            elif command in "hit":
+                pyperclip.copy(hit (data, eweapon))
+                continue
+
+        # equip weapon
+        if command in "equip weapon":
+            for key in getWeaponInfo(data):
+                print (key)
+            eweapon = input("choose weapon: ")
+            for weapname in getWeaponInfo(data):
+                if eweapon.lower() in weapname.lower():
+                    eweapon = weapname
+                    break
+            print ("You equip {}".format(eweapon))
+
+
+        elif command in "skills":
+            pyperclip.copy(skillcheck(data))
+
+        elif command in "save":
+            pyperclip.copy(save(data))
+
+
+        elif command == "exit":
+            break
+
+
 
 def rofi(data):
     # https://github.com/bcbnz/python-rofi
